061962c60ac17b14c4e1d229351293b8c989cfd4
hoomi71 <hoomi71@gmail.com>
2024-08-20 22:07:02 +0200

    destroy all resources

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
-module "virtual_networks" {
-  source = "./modules/vnet"
-}
-
-module "virtual_machine" {
-  source = "./modules/vmlinux"
-  resource_group_name = module.virtual_networks.resource_group_names
-  resource_group_location = module.virtual_networks.resource_group_location
-  network_interface_id = module.virtual_networks.network_interface_id
-  security_group_id = module.virtual_networks.security_group_id
-  depends_on = [
-    module.virtual_networks
-  ]
-}
+# module "virtual_networks" {
+#   source = "./modules/vnet"
+# }
+#
+# module "virtual_machine" {
+#   source = "./modules/vmlinux"
+#   resource_group_name = module.virtual_networks.resource_group_names
+#   resource_group_location = module.virtual_networks.resource_group_location
+#   network_interface_id = module.virtual_networks.network_interface_id
+#   security_group_id = module.virtual_networks.security_group_id
+#   depends_on = [
+#     module.virtual_networks
+#   ]
+# }
